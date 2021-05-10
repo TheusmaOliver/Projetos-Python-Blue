70d1c2e65b823a54b4f086eadcc8fa5e4f2c5be4
Matheus Rodrigues <81190214+TheusmaOliver@users.noreply.github.com>
2021-05-10 18:29:00 -0300

    Resolvendo Exercicíos em python

---
+++ Aula 10-05/Exemplo2.py
@@ -0,0 +1,11 @@
+def comp(a, b):
+    if a == b:
+        print("Números iguais")
+    else:
+        print("Números diferentes")
+
+
+n1 = int(input("Digite o primeiro número: "))
+n2 = int(input("Digite o segundo número: "))
+
+comp(n1, n2)
